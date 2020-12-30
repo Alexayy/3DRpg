a6719ee9fe676bb6b1e6d40ca14c020c9ee1e315
aaaaaaaa <aleksa.cakic@gmail.com>
2020-12-30 20:53:05 +0100

    Initial commit

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;
+    public Transform interactionTransform;
+
+    private bool isFocused = false;
+    private Transform player;
+
+    private bool hasInteracted;
+
+    public virtual void Interact()
+    {
+        // This method is ment to be overriden
+        Debug.Log("Interacting with: " + transform.name);
+    }
+
+    private void Update()
+    {
+        if (isFocused && !hasInteracted)
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if (distance <= radius)
+            {
+                hasInteracted = true;
+                Debug.Log("I N T E R A C T");
+            } 
+        }
+    }
+
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocused = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDefocused()
+    {
+        isFocused = false;
+        player = player.transform;
+        hasInteracted = false;
+    }
+    
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(interactionTransform.position, radius);
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMotor : MonoBehaviour
+{
+    private Transform target;
+    private NavMeshAgent agent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+            FaceTarget();
+        }
+    }
+
+    public void FollowTarget(Interactable newTarget)
+    {
+        agent.stoppingDistance = newTarget.radius * 0.8f;
+        agent.updateRotation = false;
+        
+        target = newTarget.interactionTransform;
+    }
+    
+    public void StopFollowTarget()
+    {
+        agent.stoppingDistance = 0f;
+        agent.updateRotation = true;
+        
+        target = null;
+    }
+
+    public void MoveToPoint(Vector3 point)
+    {
+        agent.SetDestination(point);
+    }
+
+    void FaceTarget() 
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookDirection = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookDirection, Time.deltaTime * 5f);
+    }
+}
